8853f989df3ae79fb51af4ee6f3153170a49a82d
Gallipuzza <148063488+Gallipuzza@users.noreply.github.com>
2023-11-10 09:50:22 +0100

    Started to add logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-fetch("https://danebidev.github.io/data/characters.json")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+async function play() {
+    let json = await (await fetch("https://danebidev.github.io/data/characters.json")).json();
+    if (json.question == undefined) return;
+
+    let question = document.getElementById("question");
+    let answers = document.getElementById("answers");
+
+    change(json, question, answers);
+}
+
+function change(json, question, answers) {
+    if (json.answer != undefined) {
+        document.getElementById("answer").innerHTML = "Il tuo personaggio è " + json.answer;
+        question.innerHTML = "";
+        answers.innerHTML = "";
+        return;
+    }
+
+    question.innerHTML = json.question;
+}
+
+play();
